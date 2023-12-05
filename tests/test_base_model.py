62a72d8b1a9ac04b7a8bd6ddc12ccf6ff564beb6
Avellin003 <uavellinbonaparte@gmail.com>
2023-12-05 15:30:32 +0200

    test_updates

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,1 +1,27 @@
+#!/usr/bin/env python3
+"""
+creation of a unittest for the base class
+"""
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+import uuid
 
+
+class TestBasemodel(unittest.TestCase):
+    """creation of tester"""
+    def test_init(self):
+        """function which tests the constructor of the basemodel"""
+        a = BaseModel()
+        self.assertIsInstance(a.id, str)
+        self.assertIsInstance(a.updated_at, datetime)
+        self.assertIsInstance(a.updated_at, datetime)
+    
+    def test_str_(self):
+        """method that tests for the string returned in basemodel"""
+        b = BaseModel()
+        exp = f"[BaseModel] ({b.id}) {b.__dict__}"
+        self.assertEqual(str(b), exp)
+
+if __name__ == "__main__":
+    unittest.main()
