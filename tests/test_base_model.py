bbf80123ed52b9cce581f424c5e1cc0fdadb7c5e
Avellin003 <uavellinbonaparte@gmail.com>
2023-12-10 22:07:55 +0200

    update

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -16,12 +16,13 @@ class TestBasemodel(unittest.TestCase):
         self.assertIsInstance(a.id, str)
         self.assertIsInstance(a.updated_at, datetime)
         self.assertIsInstance(a.updated_at, datetime)
-    
+
     def test_str_(self):
         """method that tests for the string returned in basemodel"""
         b = BaseModel()
         exp = f"[BaseModel] ({b.id}) {b.__dict__}"
         self.assertEqual(str(b), exp)
+
 
 if __name__ == "__main__":
     unittest.main()
